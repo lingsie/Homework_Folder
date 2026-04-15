e1810ddbaa4b1f2b1cbed3316f443727aba1a162
Lingsie <jimling0916@gmail.com>
2026-04-15 03:33:14 -0700

    v1.0

--- ETE_1151_HomeWork/Jim_Ling_ETE_1151_Homework_10/main.cpp
+++ ETE_1151_HomeWork/Jim_Ling_ETE_1151_Homework_10/main.cpp
@@ -1,133 +1,171 @@
-// bring in
 #include <iostream>
 #include <string>
 using namespace std;
+
 
 // base class
 class Pokemon {
 protected:
     int level;
     string name;
-    
+
+
 public:
-    Pokemon (const string& n, int lvl) : name(n), level(lv1) {}
-    
+    Pokemon(const string& n, int lvl) : level(lvl), name(n) {}
+
+
     virtual ~Pokemon() {}
-    
+
+
     // name
     string getName() const {
         return name;
     }
-    
-    // lv
-    string getLv() const (
+
+
+    // level
+    int getLv() const {
         return level;
     }
-    
+
+
     // pure virtual funcs
     virtual void attack() const = 0;
     virtual void defend() const = 0;
-    virtual void evolve() = 0; // no need const
+    virtual void evolve() = 0;
     virtual void display() const = 0;
 };
 
 
-// Intermediate type class, pokemon type
+// Intermediate type class
 class PokemonType : public Pokemon {
 protected:
     string type;
-    
+
+
 public:
-    PokemonType(const string& n, int lvl)
-        : PokemonType(n,  lvl, "Electric") {}
-        
-    void defend() const override {
-        coud << name << " Blocks with electric resistance." << endl;
+    PokemonType(const string& n, int lvl, const string& t)
+        : Pokemon(n, lvl), type(t) {}
+
+
+    string getType() const {
+        return type;
+    }
+
+
+    void display() const override {
+        cout << "Pokemon: " << name
+             << "\nLevel: " << level
+             << "\nType: " << type << endl;
     }
 };
+
+
+// Electric Pokemon
+class ElectricPokemon : public PokemonType {
+public:
+    ElectricPokemon(const string& n, int lvl)
+        : PokemonType(n, lvl, "Electric") {}
+
+
+    void defend() const override {
+        cout << name << " defends with electric resistance." << endl;
+    }
+};
+
 
 // Pikachu
 class Pikachu : public ElectricPokemon {
 public:
-    Pikachu(int lvl) : ElectricPokemon("Pikachu" , lvl) {}
-    
-    // attack
+    Pikachu(int lvl) : ElectricPokemon("Pikachu", lvl) {}
+
+
     void attack() const override {
-        cout << name << " uses thunder shock." << endl;
+        cout << name << " uses Thunder Shock." << endl;
     }
-    
-    // eolve
-    void attack() override {
+
+
+    void evolve() override {
         if (level >= 20) {
-            cout << name << " eolves into Raichu" << endl;
+            cout << name << " evolves into Raichu." << endl;
         } else {
-            cpit << name << " Not enough level to eolves yet." << endl;
+            cout << name << " does not have enough level to evolve yet." << endl;
         }
     }
-    
-    // info
+
+
     void display() const override {
-        cout << " ===== Pikachu info ===== " << endl;
-        PokemonType :: display
+        cout << "===== Pikachu Info =====" << endl;
+        PokemonType::display();
     }
 };
 
-// try to make a another ex
+
+// Water Pokemon
 class WaterPokemon : public PokemonType {
 public:
-    WatherPokemon(const string& n, int lvl)
+    WaterPokemon(const string& n, int lvl)
         : PokemonType(n, lvl, "Water") {}
-    
-    void defend () const override {
-        cout << name << " defend with a water shield." << endl;
+
+
+    void defend() const override {
+        cout << name << " defends with a water shield." << endl;
     }
 };
 
+
+// Squirtle
 class Squirtle : public WaterPokemon {
 public:
     Squirtle(int lvl) : WaterPokemon("Squirtle", lvl) {}
-    
-    // attack again one more time
+
+
     void attack() const override {
-        cout << name << " Use water gun" << endl;
+        cout << name << " uses Water Gun." << endl;
     }
-    
+
+
     void evolve() override {
         if (level >= 16) {
-            cout << name << " evolves into Wartortle. " << endl;
+            cout << name << " evolves into Wartortle." << endl;
         } else {
-            cout << name << " not enough level to evolve. " << endl;
+            cout << name << " does not have enough level to evolve." << endl;
         }
     }
-    
-    void display() const override {
-        cout << " ===== Squirtle Info ===== " << endl;
-        PokemonType::display;
-    )
-);
 
-// def main()
+
+    void display() const override {
+        cout << "===== Squirtle Info =====" << endl;
+        PokemonType::display();
+    }
+};
+
+
 int main() {
-    Picachu pikachu(10);
+    Pikachu pikachu(10);
     Squirtle squirtle(18);
-    
+
+
     cout << "Part 1 Inheritance" << endl;
-    pikachu.display;
+    pikachu.display();
     cout << endl;
-    
-    cout << "Part 2 Inheritance" << endl;
+
+
+    cout << "Part 2 Polymorphism" << endl;
     Pokemon* team[2];
     team[0] = &pikachu;
     team[1] = &squirtle;
-    
-    for (int i = 0; i < 2; i++) (
-        cout << "\n ### Pokemon " << i + 1 << " ###" << endl;
+
+
+    for (int i = 0; i < 2; i++) {
+        cout << "\n### Pokemon " << i + 1 << " ###" << endl;
         team[i]->display();
         team[i]->attack();
         team[i]->defend();
         team[i]->evolve();
-    )
-    
-    // end
-    return 0
-)
+    }
+
+
+    return 0;
+}
+
